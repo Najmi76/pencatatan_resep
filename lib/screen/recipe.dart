fc6f4beae3a9818276514f9374e13ba41dc22c13
labib21sa1193 <labibnurali18@gmail.com>
2023-07-16 14:15:05 +0700

    recipe.dart Fixes #36
    file recipe sudah selesai

---
+++ lib/screen/recipe.dart
@@ -0,0 +1,205 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:resep/consent/colors.dart';
+import 'package:resep/screen/home.dart';
+
+class resep extends StatelessWidget {
+  final Map<String, dynamic> resepdata;
+
+  resep({Key? key, required this.resepdata}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: Colors.white,
+              expandedHeight: 400,
+              flexibleSpace: FlexibleSpaceBar(
+                background:
+                    Image.file(File(resepdata['image']), fit: BoxFit.cover),
+              ),
+              bottom: PreferredSize(
+                preferredSize: Size.fromHeight(50),
+                child: Container(
+                  width: double.infinity,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(70),
+                      topRight: Radius.circular(70),
+                    ),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 15),
+                      Container(
+                        width: 80,
+                        height: 4,
+                        color: font,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: CircleAvatar(
+                    backgroundColor: Color.fromRGBO(250, 250, 250, 0.6),
+                    radius: 18,
+                    child: Icon(
+                      Icons.favorite_border,
+                      size: 25,
+                      color: font,
+                    ),
+                  ),
+                ),
+              ],
+              title: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: CircleAvatar(
+                  backgroundColor: Color.fromRGBO(250, 250, 250, 0.6),
+                  radius: 18,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pop(
+                        MaterialPageRoute(
+                          builder: (BuildContext context) {
+                            return Home();
+                          },
+                        ),
+                      );
+                    },
+                    child: Icon(
+                      Icons.arrow_back,
+                      size: 25,
+                      color: font,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: _getBody(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _getBody() {
+    print('panjang data ${resepdata['bahan']}');
+    return Wrap(
+      children: [
+        Container(
+          width: double.infinity,
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: Row(
+                  children: [
+                    Text(
+                      'ingredients',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: font,
+                        fontFamily: 'ro',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 15),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children:
+                        (resepdata['bahan'] as String).split(';').map((bahan) {
+                      return Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 5),
+                        child: Container(
+                          width: 100,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(1, 1),
+                                color: Colors.grey,
+                                blurRadius: 7,
+                              ),
+                            ],
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  bahan.trim(),
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: font,
+                                    fontFamily: 'ro',
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: Row(
+                  children: [
+                    Text(
+                      'resep',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: font,
+                        fontFamily: 'ro',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                child: Text(
+                  resepdata['step'],
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: font,
+                    fontFamily: 'ro',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
